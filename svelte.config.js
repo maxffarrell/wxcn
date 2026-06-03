a1c54bc9677948c5df3e3d714513256835094450
Max Farrell <maxffarrell@gmail.com>
2026-06-03 08:53:39 -0500

    Use Vercel adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsx } from 'mdsx';
 
@@ -15,7 +15,9 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			runtime: 'nodejs24.x'
+		}),
 		alias: {
 			$components: 'src/lib/components',
 			$wxcn: 'src/lib/components/wxcn'
